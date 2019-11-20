01b7baf64dbbc21a73e11d2471de33f5c4475697
splo2t <yu990410@naver.com>
2019-11-20 01:25:27 -1000

    bluetooth_test

--- safe_forklift_display/bluetooth_test.py
+++ safe_forklift_display/bluetooth_test.py
@@ -2,13 +2,18 @@ from bluetooth import *
 # Create the client socket
 client_socket=BluetoothSocket( RFCOMM )
 client_socket.connect(("98:D3:41:FD:5C:6D", 1))
-
+readData = ""
+data = ""
 while True:
-    data = client_socket.recv(1024)
+    data += str(client_socket.recv(1),"utf-8")
+    #print(data)
     if data.find("!"):
         temp = data.split("!")
-        for x in range(0,len(temp)-1):
-            readData += x
+        for x in range(0,len(temp)):
+            readData += str(temp[x])
         data = temp[len(temp)-1]
-print "Finished"
+        print(readData)
+        readData = ""
+
+print("Finished")
 client_socket.close()
